e19f3df46a4a0ae4783151aace518bb4eb9adb69
ayoade <tomiayoade2@gmail.com>
2019-04-25 18:11:12 +0100

    New changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './assests/css/style.css'
+import './assests/lib/bootstrap/css/bootstrap.min.css'
+import './assests/lib/animate/animate.min.css'
+import './assests/lib/lightbox/css/lightbox.min.css'
+import './assests/lib/owlcarousel/assets/owl.carousel.min.css'
+import './assests/lib/font-awesome/css/font-awesome.min.css'
+import './assests/lib/ionicons/css/ionicons.min.css'
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
